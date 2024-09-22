9f6c59843e8905b1e6de86c5b4935dbf2b56fcf2
minhtrifit <minhtri.fit@gmail.com>
2024-09-22 10:45:30 +0700

    complete noti box

--- src/components/NotificationBtn.tsx
+++ src/components/NotificationBtn.tsx
@@ -1,12 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { CHAT_TYPE } from "../types/chat.type";
+import { USER_TYPE } from "../types/user.type";
+
 import { IoIosNotificationsOutline } from "react-icons/io";
+import { getLastestChat } from "../utils/utils";
 
 const NotificationBtn = () => {
+  const chats = useSelector<RootState, CHAT_TYPE[]>(
+    (state) => state.chats.chats
+  );
+
+  const friends = useSelector<RootState, USER_TYPE[]>(
+    (state) => state.users.friends
+  );
+
+  const currentUser = useSelector<RootState, USER_TYPE | null>(
+    (state) => state.users.currentUser
+  );
+
   const [isOpen, setIsOpen] = useState(false);
+  const [notis, setNotis] = useState<{ user: USER_TYPE; chat: CHAT_TYPE }[]>(
+    []
+  );
 
   const toggleDropdown = () => {
     setIsOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    const filterNotis: { user: USER_TYPE; chat: CHAT_TYPE }[] = [];
+
+    for (let i = 0; i < friends?.length; ++i) {
+      const friendId = friends[i]?.id;
+
+      if (currentUser?.id && friendId) {
+        const filterChats = getLastestChat(chats, currentUser?.id, friendId);
+
+        if (filterChats[filterChats.length - 1]) {
+          filterNotis.push({
+            user: friends[i],
+            chat: filterChats[filterChats.length - 1],
+          });
+        }
+      }
+
+      setNotis(filterNotis);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="relative">
@@ -18,7 +63,7 @@ const NotificationBtn = () => {
         <IoIosNotificationsOutline size={30} />
       </button>
       <div
-        className={`absolute right-0 mt-2 w-[350px] bg-white dark:bg-primary-gray
+        className={`absolute right-[-160px] sm:right-0 mt-2 w-[350px] bg-white dark:bg-primary-gray
                     text-black dark:text-white rounded-md shadow-lg z-10
                     transition-transform duration-300 ease-in-out 
                     ${
@@ -29,7 +74,35 @@ const NotificationBtn = () => {
         style={{ transform: isOpen ? "translateY(0)" : "translateY(-8px)" }}
       >
         <div className="p-4">
-          <p>Notification information</p>
+          <h1 className="text-center font-bold text-md">Notifications</h1>
+          <div className="mt-5 w-full max-h-[300px] overflow-y-auto flex flex-col gap-y-3">
+            {notis?.map((noti: { user: USER_TYPE; chat: CHAT_TYPE }) => {
+              return (
+                <div
+                  key={uuidv4()}
+                  className="p-2 rounded-sm w-full flex items-center justify-start gap-x-3
+                              hover:cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-700"
+                >
+                  <div
+                    className="w-[40px] h-[40px] bg-light-gray dark:bg-secondary-gray rounded-full
+                                flex items-center justify-center"
+                  >
+                    {noti?.user?.name
+                      ? noti?.user?.name.charAt(0)
+                      : "undefined"}
+                  </div>
+                  <div className="flex flex-col gap-y-1">
+                    <span className="text-xs text-zinc-400">
+                      {noti?.chat?.sendAt ? noti?.chat?.sendAt : "undefined"}
+                    </span>
+                    <span className="text-sm max-w-[200px] truncate">
+                      {noti?.chat?.message ? noti?.chat?.message : "undefined"}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,8 @@ import ToggleTheme from "../components/ToggleTheme";
 import DrawerBtn from "../components/DrawerBtn";
 import NotificationBtn from "../components/NotificationBtn";
 
+import { IoChatbubblesSharp } from "react-icons/io5";
+
 const HomePage = () => {
   return (
     <div
@@ -9,9 +11,9 @@ const HomePage = () => {
                   bg-gray-100 dark:bg-secondary-gray"
     >
       <div className="w-full flex items-center justify-between">
-        <div className="flex flex-col sm:flex-row items-start sm:items-end gap-3">
-          <img className="w-[150px]" src="/assets/nexlab.svg" />
-          <span className="text-xl text-gray-600 dark:text-sky-500 font-bold">
+        <div className="flex flex-col sm:flex-row items-center gap-3">
+          <IoChatbubblesSharp size={40} />
+          <span className="hidden sm:block text-xl text-gray-600 dark:text-sky-500 font-bold">
             Chat Application
           </span>
         </div>
